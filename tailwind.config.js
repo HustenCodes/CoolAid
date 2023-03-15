c3c8de5baa6b60808f4e94f33fe9a107c3b78ef1
HustenCodes <hustenlafleur@outlook.com>
2023-03-14 18:58:33 -0700

    Init Commit :)

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
